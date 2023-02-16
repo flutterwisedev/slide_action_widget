a373fda341714a6ede27e723edaddfe20bfcb77e
Flutterwise <flutterwise.dev@gmail.com>
2023-02-16 16:25:10 -0600

    update example code

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -28,12 +28,57 @@ class Body extends StatelessWidget {
         padding: const EdgeInsets.all(8),
         child: Container(
           constraints: const BoxConstraints(maxWidth: 500),
-          child: SlideActionWidget(
-            backgroundText: 'Purchasing...',
-            foregroundText: 'Slide to purchase',
-            onComplete: () {
-              print('Purchased');
-            },
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              // Example #1
+              SlideActionWidget(
+                backgroundText: 'Purchasing...',
+                foregroundText: 'Slide to purchase',
+                onComplete: () {
+                  print('Purchased');
+                },
+              ),
+              const SizedBox(height: 12),
+              // Example #2
+              SlideActionWidget(
+                backgroundText: 'Sending',
+                foregroundText: 'Slide to send',
+                onComplete: () {
+                  print('Sent');
+                },
+              ),
+              const SizedBox(height: 12),
+
+              // Example #3
+              SlideActionWidget.custom(
+                background: Container(
+                  color: Colors.red.shade100,
+                  child: Center(
+                    child: Text(
+                      'Deleting...',
+                      style: TextStyle(
+                        color: Colors.red.shade900,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                  ),
+                ),
+                foreground: Container(
+                  color: Colors.orangeAccent.shade100,
+                  child: const Center(child: Text('Slide to delete')),
+                ),
+                thumb: SlideActionThumb(
+                  width: 60,
+                  color: Colors.red.shade300,
+                  borderRadius: BorderRadius.circular(12),
+                  icon: const Icon(Icons.delete),
+                ),
+                onComplete: () {
+                  print('Deleted');
+                },
+              ),
+            ],
           ),
         ),
       ),
